14fdbec2451396c4d0ff8967038ae2b8420e58e3
Mihail Marian <mihailmarian12@gmail.com>
2021-02-06 09:48:21 +0000

    moving along the course

--- myfirst.js
+++ myfirst.js
@@ -1,8 +1,13 @@
 let http = require("http");
+let dt = require("./myfirstmodule");
 
 http
   .createServer((req, res) => {
     res.writeHead(200, { "Content-Type": "text/html" });
+    res.write(
+      "<div>The date and time are currently: " + dt.myDateTime() + "</div>"
+    );
+    res.write(req.url + "\n");
     res.end("Hello World!");
   })
   .listen(8080);
